dd2dca405dc8b5f0bfa418944eb7b2d5bf07825f
trentv4 <trentvanslyke@gmail.com>
2017-03-15 04:37:35 -0400

    Add coal dust

--- src/main/java/net/trentv/gases/init/GasesObjects.java
+++ src/main/java/net/trentv/gases/init/GasesObjects.java
@@ -11,10 +11,12 @@ public class GasesObjects
 {
 	public static final GasType NATURAL_GAS = new GasType("natural", 0x6F7F6F, 2, 1, Combustibility.FLAMMABLE).setCreativeTab(GasesFramework.CREATIVE_TAB);
 	public static final GasType RED_GAS = new GasType("red", 0x7F0000, 2, -1, Combustibility.EXPLOSIVE).setCreativeTab(GasesFramework.CREATIVE_TAB);
+	public static final GasType COAL_DUST = new GasType("coal_dust", 0x000000, 2, -1, Combustibility.EXPLOSIVE).setCreativeTab(GasesFramework.CREATIVE_TAB);
 
 	public static void init()
 	{
 		GFRegistrationAPI.registerGasType(NATURAL_GAS, new ResourceLocation(Gases.MODID, "gas_" + NATURAL_GAS.name));
 		GFRegistrationAPI.registerGasType(RED_GAS, new ResourceLocation(Gases.MODID, "gas_" + RED_GAS.name));
+		GFRegistrationAPI.registerGasType(COAL_DUST, new ResourceLocation(Gases.MODID, "gas_" + COAL_DUST.name));
 	}
 }
