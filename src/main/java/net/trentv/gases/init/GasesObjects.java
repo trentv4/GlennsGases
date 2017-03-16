7564d71858c34eaea739e1d1045e413ab614f717
trentv4 <trentvanslyke@gmail.com>
2017-03-15 22:50:40 -0400

    Set dissipation and cohesion rates for steam and coal dust

--- src/main/java/net/trentv/gases/init/GasesObjects.java
+++ src/main/java/net/trentv/gases/init/GasesObjects.java
@@ -11,8 +11,8 @@ public class GasesObjects
 {
 	public static final GasType NATURAL_GAS = new GasType("natural", 0x6F7F6F, 2, 1, Combustibility.FLAMMABLE).setCreativeTab(GasesFramework.CREATIVE_TAB);
 	public static final GasType RED_GAS = new GasType("red", 0x7F0000, 2, -1, Combustibility.EXPLOSIVE).setCreativeTab(GasesFramework.CREATIVE_TAB);
-	public static final GasType COAL_DUST = new GasType("coal_dust", 0x000000, 2, -1, Combustibility.EXPLOSIVE).setCreativeTab(GasesFramework.CREATIVE_TAB);
-	public static final GasType STEAM = new GasType("steam", 0xFFFFFF, 2, 1, Combustibility.EXPLOSIVE).setCreativeTab(GasesFramework.CREATIVE_TAB);
+	public static final GasType COAL_DUST = new GasType("coal_dust", 0x000000, 2, 0, Combustibility.EXPLOSIVE).setCohesion(8).setDissipation(2, 4).setCreativeTab(GasesFramework.CREATIVE_TAB);
+	public static final GasType STEAM = new GasType("steam", 0xFFFFFF, 2, 1, Combustibility.EXPLOSIVE).setCohesion(2).setDissipation(4, 2).setCreativeTab(GasesFramework.CREATIVE_TAB);
 
 	public static void init()
 	{
